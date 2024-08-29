e7a13fd5cde9dfcd62fa8e1714889c0f5b7f8833
mythilisaran <mythilisaran138@gmail.com>
2024-08-29 20:39:16 +0530

    Create 7led.ino

---
+++ 7led.ino
@@ -0,0 +1,39 @@
+//https://wokwi.com/projects/407565832756371457
+void setup() {
+  // put your setup code here, to run once:
+pinMode(13,OUTPUT);
+pinMode(12, OUTPUT);
+pinMode(11, OUTPUT);
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+digitalWrite(13, HIGH);
+delay(100);
+digitalWrite(13, LOW);
+delay(100);
+digitalWrite(12, HIGH);
+delay(100);
+digitalWrite(12, LOW);
+delay(100);
+digitalWrite(11, HIGH);
+delay(100);;
+digitalWrite(11, LOW);
+delay(100);
+digitalWrite(10, HIGH);
+delay(100);
+digitalWrite(10, LOW);
+delay(100);
+digitalWrite(9, HIGH);
+delay(100);
+digitalWrite(9, LOW);
+delay(100);
+digitalWrite(8, HIGH);
+delay(100);;
+digitalWrite(8, LOW);
+delay(100);
+digitalWrite(7, HIGH);
+delay(100);;
+digitalWrite(7, LOW);
+delay(100);
+}
